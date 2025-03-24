1458f4e1acc16690a2e299cd6a1d190a22cedf6e
Kehinde Gilbert <kennyvola07@gmail.com>
2025-03-24 03:15:57 -0500

    Add EDA and app updates

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ st.title("My Streamlit App")
 
 st.header("Vehicle Sales Dashboard")
 
+st.text("This project aims to provide you with additional practice on common software engineering tasks.")
+st.text("These tasks will augment and complement your data skills, and make you a more attractive job candidate to potential employers.")
+
+df = pd.read_csv('vehicles_us.csv')
+
    # Histogram with unique key
 fig_hist = px.histogram(df, x='price', title='Price Distribution')
 st.plotly_chart(fig_hist, key='histogram')
@@ -18,4 +23,6 @@ st.plotly_chart(fig_scatter, key='scatter')
 if st.checkbox('Show raw data', key='raw'):
        st.write(df)
        
-
+fig = px.scatter(df, x='model_year', y='price', title='All Data Sample')
+st.plotly_chart(fig)
+ 
